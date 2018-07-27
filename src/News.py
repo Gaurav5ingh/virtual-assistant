e64dfac9170ca12db1c4f1dafb354b21e1d845e1
Gaurav5ingh <36332787+Gaurav5ingh@users.noreply.github.com>
2018-07-27 20:41:36 +0530

    Create News.py

---
+++ src/News.py
@@ -0,0 +1,31 @@
+import bs4
+from bs4 import BeautifulSoup 
+from urllib.request import urlopen
+
+from Voice import talk
+from MyCommand import myCommands  
+from TLD import get_tld
+
+def news_headlines():
+
+    tld = get_tld()
+
+    try:  
+        news_url = "https://news.google"+tld+"/news/rss"
+        reader = urlopen(news_url)
+        page = reader.read()
+        reader.close()
+    except:
+        news_url = "https://news.google"+".co"+tld+"/news/rss"
+        reader = urlopen(news_url)
+        page = reader.read()
+        reader.close()
+        
+    soup = BeautifulSoup(page,"xml")
+    news_list = soup.findAll("item")
+    # Print news title, url and publish date
+    for news in news_list:
+        print(news.title.text)
+        #print(news.link.text)
+        print ("-"*60)
+
