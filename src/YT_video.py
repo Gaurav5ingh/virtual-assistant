f64453f4e0ba5bd6c9e82a4f34b955ca94108ca2
Gaurav5ingh <36332787+Gaurav5ingh@users.noreply.github.com>
2018-07-27 20:36:43 +0530

    Create YT_video.py

---
+++ src/YT_video.py
@@ -0,0 +1,21 @@
+from pytube import YouTube
+import webbrowser
+from G_search_url import get_url
+
+def video_dwnld():
+
+    query = input("Which do you want to download?")
+
+    url = get_url(query + 'youtube') 
+    
+    yt = YouTube(url)
+
+    stream = yt.streams.first()
+
+    stream.download()
+
+
+def video_stream():
+
+    query = input("What do you want to play?")
+    webbrowser.open(url)
