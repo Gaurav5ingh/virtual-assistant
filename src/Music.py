9e3c6d75285d7a5568e73c5d343b1ec76978540c
Gaurav5ingh <36332787+Gaurav5ingh@users.noreply.github.com>
2018-07-27 20:35:27 +0530

    Create Music.py

---
+++ src/Music.py
@@ -0,0 +1,24 @@
+import webbrowser
+from G_search_url import get_url
+#from selenium.webdriver import Firefox
+#from selenium.webdriver.firefox.options import Options
+
+def play_music():
+    song = input("What do you want to listen:")
+    query = 'play ' + song + ' from gaana.com'
+    url = get_url(query)
+    print('playing...')
+    webbrowser.open(url)
+
+#for start playing song without opening any window/tab  
+'''
+opts = Options()
+opts.set_headless()
+browser = Firefox(options=opts)
+browser.get(url)
+try :
+    browser.find_element_by_class('play').click()  
+    browse.find_element_by_class('pause').click()
+except AttributeError:
+    print("playing...")
+'''
