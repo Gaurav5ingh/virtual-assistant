3006a58b85a1ec34c61d6121d41ad84b698ae082
Gaurav5ingh <36332787+Gaurav5ingh@users.noreply.github.com>
2018-07-27 20:37:28 +0530

    Create GoogleSearch.py

---
+++ src/GoogleSearch.py
@@ -0,0 +1,12 @@
+from G_search_url import get_url
+import webbrowser
+from Voice import talk
+from MyCommand import myCommands 
+
+def google_search(query):
+    
+    try:
+        url = get_url(query)
+        webbrowser.open(url)
+    except:
+        talk("Sorry, something went wrong!")
